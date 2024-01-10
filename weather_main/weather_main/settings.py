a60ce116794476a328b4fb1041877fc082351d0a
Shivanshu <craziest.oliyhan13@gmail.com>
2024-01-11 02:08:48 +0530

    downgrading to 3.9

--- weather_main/weather_main/settings.py
+++ weather_main/weather_main/settings.py
@@ -11,9 +11,9 @@ API_KEY = os.getenv('API_KEY')
 SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
